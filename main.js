ccf8282b3bc74e20a662e83dd52175973f23c974
Shawn Truesdale <shawnctruesdale@gmail.com>
2020-09-09 19:20:15 -0600

    Radio button redo before styling

--- main.js
+++ main.js
@@ -1,31 +1,16 @@
 var activities = [];
-
-var studyButton = document.querySelector('.study-button');
-var meditateButton = document.querySelector('.meditate-button');
-var exerciseButton = document.querySelector('.exercise-button');
 var startActivityButton = document.querySelector('.start-activity-button');
 var descriptionInput = document.querySelector('.description-input');
 var minuteInput = document.querySelector('.minutes');
 var secondInput = document.querySelector('.seconds');
 
-// var categoryButton = document.querySelectorAll('.category-button')
-
-startActivityButton.addEventListener('click', startActivity);
-
-studyButton.addEventListener('click', function () {
-  studyButton.classList.toggle('study-highlighted');
-});
-
-meditateButton.addEventListener('click', function () {
-  meditateButton.classList.toggle('meditate-highlighted');
-});
-
-exerciseButton.addEventListener('click', function () {
-  exerciseButton.classList.toggle('exercise-highlighted');
-});
-
 minuteInput.addEventListener('keyup', checkNumber);
 secondInput.addEventListener('keyup', checkNumber);
+
+function startActivity() {
+  var checkedButton = document.querySelector('input[name="cat-group"]:checked').value;
+  createActivity(checkedButton);
+}
 
 function checkNumber() {
   var minutes = parseInt(minuteInput.value);
@@ -35,20 +20,12 @@ function checkNumber() {
   }
 }
 
-function startActivity() {
+function createActivity(category) {
   var newActivity = new Activity(
-    'category',
+    category,
     descriptionInput.value,
     minuteInput.value,
     secondInput.value
   );
   activities.push(newActivity);
 }
-
-
-
-// for (var i = 0; i < categoryButton.length; i++) {
-//   categoryButton[i].addEventListener('click', function() {
-//     categoryButton.classList.add('highlighted')
-//   })
-// }
