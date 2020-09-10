3121f1d40ad8ce865e878a89368d3a26ab3d458c
Bailey Dunning <bwdunning@gmail.com>
2020-09-09 20:42:47 -0600

    Add onclick functions to buttons & add highlightSelected()

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var selectedCategory;
 var activities = [];
 
 var studyButton = document.querySelector('.study-button');
@@ -8,24 +9,52 @@ var descriptionInput = document.querySelector('.description-input');
 var minuteInput = document.querySelector('.minutes');
 var secondInput = document.querySelector('.seconds');
 
-// var categoryButton = document.querySelectorAll('.category-button')
-
 startActivityButton.addEventListener('click', startActivity);
-
-studyButton.addEventListener('click', function () {
-  studyButton.classList.toggle('study-highlighted');
-});
-
-meditateButton.addEventListener('click', function () {
-  meditateButton.classList.toggle('meditate-highlighted');
-});
-
-exerciseButton.addEventListener('click', function () {
-  exerciseButton.classList.toggle('exercise-highlighted');
-});
-
 minuteInput.addEventListener('keyup', checkNumber);
 secondInput.addEventListener('keyup', checkNumber);
+
+studyButton.onclick = function() {
+  updateCategory(studyButton);
+}
+
+meditateButton.onclick = function() {
+  updateCategory(meditateButton);
+}
+
+exerciseButton.onclick = function() {
+  updateCategory(exerciseButton);
+}
+
+function updateCategory(button) {
+  selectedCategory = button.value;
+  highlightSelected();
+  console.log(selectedCategory);
+}
+
+function highlightSelected() {
+  if (selectedCategory === 'study') {
+    studyButton.classList.add('study-highlighted');
+    document.getElementById('study-icon').src = "./assets/study-active.svg"
+    meditateButton.classList.remove('meditate-highlighted');
+    exerciseButton.classList.remove('exercise-highlighted');
+  }
+  if (selectedCategory === 'meditate') {
+    meditateButton.classList.add('meditate-highlighted');
+    document.getElementById('meditate-icon').src = "./assets/meditate-active.svg"
+    studyButton.classList.remove('study-highlighted');
+    exerciseButton.classList.remove('exercise-highlighted');
+  }
+  if (selectedCategory === 'exercise') {
+    exerciseButton.classList.add('exercise-highlighted');
+    document.getElementById('exercise-icon').src = "./assets/exercise-active.svg"
+    studyButton.classList.remove('study-highlighted');
+    meditateButton.classList.remove('meditate-highlighted');
+  }
+}
+
+function highlightIcon() {
+
+}
 
 function checkNumber() {
   var minutes = parseInt(minuteInput.value);
@@ -35,20 +64,14 @@ function checkNumber() {
   }
 }
 
-function startActivity() {
+function startActivity(category) {
   var newActivity = new Activity(
-    'category',
+    selectedCategory,
     descriptionInput.value,
     minuteInput.value,
     secondInput.value
   );
   activities.push(newActivity);
+  console.log(newActivity);
+  console.log(activities)
 }
-
-
-
-// for (var i = 0; i < categoryButton.length; i++) {
-//   categoryButton[i].addEventListener('click', function() {
-//     categoryButton.classList.add('highlighted')
-//   })
-// }
